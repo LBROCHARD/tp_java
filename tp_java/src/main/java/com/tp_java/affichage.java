01e9cfdda0a7b9478b6311d95dc0f1f43f2e3a5f
LBROCHARD <73283488+LBROCHARD@users.noreply.github.com>
2021-09-27 16:34:41 +0200

    update affichage

--- tp_java/src/main/java/com/tp_java/affichage.java
+++ tp_java/src/main/java/com/tp_java/affichage.java
@@ -7,24 +7,28 @@ import java.io.IOException;
 public class affichage {
     
     public static int Menue(){
-        System.out.println("Do you want to play :");
-        System.out.println("1. online ");
-        System.out.println("2. local ");
+        affichage.Print("Do you want to play :");
+        affichage.Print("1. online ");
+        affichage.Print("2. local ");
 
         InputStreamReader isr = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(isr);
         try {
             int awnser =Integer.parseInt(br.readLine());  
-            
+
             if(awnser == 1 || awnser == 2){
                 return awnser;
             } else {
-                System.err.println("this choice is not correct");
+                affichage.PrintError("Awnser must be in the list");
                 return Menue();
             }
 
+        } catch(NumberFormatException e) {
+            affichage.PrintError("Awnser must be a number : " + e.getMessage() );
+            return Menue();
+
         } catch(IOException e) {
-            System.err.println("this awnser is wrong" + e.getMessage() );
+            affichage.PrintError("Error : " + e.getMessage() );
             return Menue();
         }
 
@@ -32,22 +36,23 @@ public class affichage {
 
     }
 
-    public static void Show(){
-        System.out.println("show");
+    public static void Print(String message){
+        System.out.println(message);
+    }
 
+    public static void PrintError(String message){
+        System.out.println("ERROR :" + message);
     }
 
     public static void BoardAndAsk(){
-        System.out.println("|        |");
-        System.out.println("|        |");
-        System.out.println("|        |");
-        System.out.println("|        |");
-        System.out.println("|        |");
-        System.out.println("|        |");
-        System.out.println("----------");
-        System.out.println(" 12345678 ");
+        affichage.Print("|        |");
+        affichage.Print("|        |");
+        affichage.Print("|        |");
+        affichage.Print("|        |");
+        affichage.Print("|        |");
+        affichage.Print("|        |");
+        affichage.Print("----------");
+        affichage.Print(" 12345678 ");
 
-
-        
     }
 }
--- tp_java/src/main/java/com/tp_java/jeu.java
+++ tp_java/src/main/java/com/tp_java/jeu.java
@@ -1,24 +1,83 @@
 package com.tp_java;
 
+import java.nio.channels.SocketChannel;
+import java.nio.channels.ServerSocketChannel;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.Random;
+
 public class jeu {
+
+    static String color = "Empty";
     
     public static void Launch(){
         int awnser = affichage.Menue();
 
         if(awnser == 1){
-            
+            Reseau();
         } else {
-
+            Local();
         }
     }
 
     public static void Reseau(){
+        affichage.Print("! Online is a work in progress !");
+        affichage.Print("please select something else in the menu");
+        Launch();
 
     }
 
-    public static double RandomPlayer(){
-        double rndm = Math.random()*(1 - 0 + 1 ) + 0;   
-        return rndm;
+    private static void HostGame(){
+        try {
+            affichage.Print("Hosting a game ...");
+            ServerSocketChannel serverSocket = ServerSocketChannel.open();
+            serverSocket.socket().bind(new InetSocketAddress(8000));
+            SocketChannel socketChannel = serverSocket.accept();
+            affichage.PrintError("Listening on chanell 8000");
+
+
+        } catch (IOException e) {
+            affichage.PrintError("Error while trying to host game");
+        }
+
+    }
+
+    private static void JoinGame(){
+        try {
+            affichage.Print("Joining a game ...");
+            SocketChannel clientSocket = SocketChannel.open();
+            clientSocket.connect( new InetSocketAddress("localhost", 8000));
+            affichage.PrintError("Sucessfully joined a game !");
+
+        } catch (IOException e) {
+            affichage.PrintError("Error while trying to join game");
+        }
+    }
+
+    public static void Local(){
+        affichage.Print("launching local mod ");
+        color = RandomPlayer();
+        affichage.Print("the first player is : " + color);
+    }
+
+    public static String RandomPlayer(){
+        Random r = new Random();
+		int rndm = r.nextInt((2 - 1) + 1) + 1;
+        // double rndm = Math.random()*(2 - 1 + 2 ) + 1;   
+        if (rndm == 1) {
+            String color = "Red";
+            return color;
+
+
+        } else if (rndm == 2) {
+            String color = "Yellow";
+            return color;
+
+        } else {
+            affichage.PrintError("error in the selection of player, actually player = " + rndm);
+            System.exit(0);
+            return "error";
+        }
     }
 
     public static void Tour(){
@@ -26,8 +85,4 @@ public class jeu {
         
     }
 
-    public static void Local(){
-
-
-    }
 }
